097f39d78beaa53da99f4c78d2d2e38c8ac9b6d0
romel <romelmahmud.dev@gmail.com>
2024-01-25 22:25:50 +0600

    cart add item remove item implement

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,24 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import MovieList from "./components/cine/MovieList";
-import { MovieContext } from "./context/index";
+import { MovieContext, ThemeContext } from "./context/index";
 
 function App() {
   const [cartData, setCartData] = useState([]);
   return (
     <>
-      <MovieContext.Provider value={{ cartData, setCartData }}>
-        <Header />
-        <main>
-          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-            <Sidebar />
-            <MovieList />
-          </div>
-        </main>
-        <Footer />
-      </MovieContext.Provider>
+      <ThemeContext.provider>
+        <MovieContext.Provider value={{ cartData, setCartData }}>
+          <Header />
+          <main>
+            <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+              <Sidebar />
+              <MovieList />
+            </div>
+          </main>
+          <Footer />
+        </MovieContext.Provider>
+      </ThemeContext.provider>
     </>
   );
 }
--- src/components/cine/CartDetails.jsx
+++ src/components/cine/CartDetails.jsx
@@ -6,7 +6,16 @@ import { getImageUrl } from "../../utils/cine-utility";
 
 const CartDetails = ({ onCancel }) => {
   const { cartData, setCartData } = useContext(MovieContext);
-  console.log(cartData);
+
+  const handleCartDelete = (e, itemId) => {
+    e.preventDefault();
+
+    const filteredItem = cartData.filter((item) => {
+      return item.id !== itemId;
+    });
+    setCartData([...filteredItem]);
+  };
+
   return (
     <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[790px] p-4 max-h-[90vh] overflow-auto">
@@ -15,34 +24,41 @@ const CartDetails = ({ onCancel }) => {
             Your Carts
           </h2>
           <div className="space-y-8 lg:space-y-12 max-h-[450px] overflow-auto mb-10 lg:mb-14">
-            {cartData.map((item) => (
-              <div className="grid grid-cols-[1fr_auto] gap-4">
-                <div className="flex items-center gap-4">
-                  <img
-                    className="rounded overflow-hidden"
-                    src={getImageUrl(item.cover)}
-                    alt={item.title}
-                    width={"100px"}
-                    height={"100px"}
-                  />
-                  <div>
-                    <h3 className="text-base md:text-xl font-bold">
-                      {item.title}
-                    </h3>
-                    <p className="max-md:text-xs text-[#575A6E]">
-                      {item.genre}
-                    </p>
-                    <span className="max-md:text-xs">${item.price}</span>
+            {cartData.length === 0 ? (
+              <p className="text-3xl">Cart is empty</p>
+            ) : (
+              cartData.map((item) => (
+                <div key={item.id} className="grid grid-cols-[1fr_auto] gap-4">
+                  <div className="flex items-center gap-4">
+                    <img
+                      className="rounded overflow-hidden"
+                      src={getImageUrl(item.cover)}
+                      alt={item.title}
+                      width={"100px"}
+                      height={"100px"}
+                    />
+                    <div>
+                      <h3 className="text-base md:text-xl font-bold">
+                        {item.title}
+                      </h3>
+                      <p className="max-md:text-xs text-[#575A6E]">
+                        {item.genre}
+                      </p>
+                      <span className="max-md:text-xs">${item.price}</span>
+                    </div>
+                  </div>
+                  <div className="flex justify-between gap-4 items-center">
+                    <button
+                      className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white"
+                      onClick={(e) => handleCartDelete(e, item.id)}
+                    >
+                      <img className="w-5 h-5" src={Delete} alt="delete" />
+                      <span className="max-md:hidden">Remove</span>
+                    </button>
                   </div>
                 </div>
-                <div className="flex justify-between gap-4 items-center">
-                  <button className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white">
-                    <img className="w-5 h-5" src={Delete} alt="" />
-                    <span className="max-md:hidden">Remove</span>
-                  </button>
-                </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
           <div className="flex items-center justify-end gap-2">
             <a
